f377395b5b503b0ca24b0f5822b16ac938fda452
mohdnazim1-admin <mohd.nazim@cxltechnologies.com>
2024-08-07 09:35:20 +0530

    deploy on vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@fortawesome': '/node_modules/@fortawesome',
+//       '@coreui/react': '/node_modules/@coreui/react',
+//     },
+//   },
+//   build: {
+//     outDir: 'dist'
+//   }
+// })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@fortawesome': '/node_modules/@fortawesome',
+//       '@coreui/react': '/node_modules/@coreui/react',
+//     },
+//   },
+//   build: {
+//     rollupOptions: {
+//       external: ['bootstrap'],
+//     },
+//     outDir: 'dist',
+//   },
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: []
+    }
+  }
+});
+
